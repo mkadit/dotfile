e3cb1499743068c47857c1046da8da4e6f997e44
mkadit <mkaditya00@gmail.com>
2021-12-16 00:06:37 +0700

    Add backup data script

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -1,100 +1,30 @@
--- local prettier = {
---     function()
---         return {
---             exe = "prettier",
---             args = {
---                 "--stdin-filepath",
---                 vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
---                 "--config",
---                 os.getenv "HOME" .. "/.config/nvim/.prettierc",
---             },
---             stdin = true,
---         }
---     end,
--- }
-
--- local white_space = {
-
---     function()
---         return { exe = "sed", args = { "-i 's/[ \t]*$//'" }, stdin = true }
---     end,
--- }
-
--- local stylua = {
---     function()
---         return {
---             exe = "stylua",
---             args = {
---                 "--config-path " .. os.getenv "HOME" .. "/.config/nvim/stylua.toml",
---                 "-",
---             },
---             stdin = true,
---         }
---     end,
--- }
-
--- local python = {
---     -- autopep8
---     function()
---         return {
---             exe = "black",
---             args = {
---                 "-",
---             },
---             stdin = true,
---         }
---     end,
--- }
-
--- local rust = {
---     function()
---         return { exe = "rustfmt", args = { "--emit=stdout" }, stdin = true }
---     end,
--- }
-
--- require("formatter").setup {
---     logging = false,
---     filetype = {
---         rust = rust,
---         lua = stylua,
---         python = python,
---         html = prettier,
---         htmldjango = prettier,
---         css = prettier,
---         javascript = prettier,
---         markdown = prettier,
---         text = white_space,
---     },
--- }
-
-
-local null_ls = require("null-ls")
+local null_ls = require "null-ls"
 
 vim.env.PRETTIERD_DEFAULT_CONFIG = vim.fn.stdpath "config" .. "/.prettierrc"
 local sources = {
-    null_ls.builtins.formatting.stylua.with{
+    null_ls.builtins.formatting.stylua.with {
         args = {
             "--config-path",
-            vim.fn.stdpath("config") .. "/stylua.toml",
-            "-"
-        }
+            vim.fn.stdpath "config" .. "/stylua.toml",
+            "-",
+        },
     },
-    null_ls.builtins.formatting.prettierd.with{
+    null_ls.builtins.formatting.prettierd.with {
         filetypes = {
-          "css",
-          "html",
-          "javascript",
-          "javascriptreact",
-          "javascript.jsx",
-          "json",
-          "jsonc",
-          "markdown",
-          "svelte",
-          "typescript",
-          "typescriptreact",
-          "vue",
-          "yaml",
-        }
+            "css",
+            "html",
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "json",
+            "jsonc",
+            "markdown",
+            "svelte",
+            "typescript",
+            "typescriptreact",
+            "vue",
+            "yaml",
+        },
     },
     null_ls.builtins.formatting.autopep8,
     null_ls.builtins.formatting.shfmt,
@@ -106,5 +36,7 @@ local sources = {
     -- null_ls.builtins.code_actions.gitsigns,
 }
 
-null_ls.config({ debounce = 150,sources = sources })
-require("lspconfig")["null-ls"].setup({})
+require("null-ls").setup {
+    debounce = 150,
+    sources = sources,
+}
