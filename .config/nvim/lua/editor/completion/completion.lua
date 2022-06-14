d9204ea815cfa467b8a41303a8edb5633171a9e1
mkadit <mkaditya00@gmail.com>
2022-06-14 21:58:45 +0700

    Change mapping

--- .config/nvim/lua/editor/completion/completion.lua
+++ .config/nvim/lua/editor/completion/completion.lua
@@ -1,73 +1,98 @@
 -- nvim-cmp setup
 local cmp = require "cmp"
 local ls = require "luasnip"
---TODO: FIx mapping to better jump between snippets
 
 -- cmp configuration
 local options = {
-    snippet = {
-        expand = function(args)
-            require'luasnip'.lsp_expand(args.body)
-        end,
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-x><C-o>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<C-l>"] = function()
+      if ls.choice_active() then
+        ls.change_choice(1)
+      end
+    end,
+    ["<C-h>"] = function()
+      if ls.choice_active() then
+        ls.change_choice(-1)
+      end
+    end,
+    ["<C-k>"] = function()
+      if ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+      end
+    end,
+    ["<C-j>"] = function()
+      if ls.jumpable(-1) then
+        ls.jump(-1)
+      end
+    end,
+    ["<C-n>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
+      else
+        fallback()
+      end
+    end,
+    ["<C-p>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item { behavior = cmp.SelectBehavior.Select }
+      else
+        fallback()
+      end
+    end,
+    ["<CR>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
     },
-    mapping = {
-        ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
-        ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = function(fallback)
-            ls.expand_or_jump()
-        end,
-        ["<C-x><C-o>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
-        ["<Tab>"] = function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
-            elseif ls.expand_or_jumpable() then
-                ls.expand_or_jump()
-            else
-                fallback()
-            end
-        end,
-        ["<S-Tab>"] = function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item { behavior = cmp.SelectBehavior.Select }
-            elseif ls.jumpable(-1) then
-                ls.jump(-1)
-            else
-                fallback()
-            end
-        end,
-    },
-    sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "path" },
-        { name = "latex_symbols" },
-    },
-    formatting = {
-        format = function(entry, vim_item)
-            -- fancy icons and a name of kind
-            vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
+    ["<Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
+      else
+        fallback()
+      end
+    end,
+    ["<S-Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item { behavior = cmp.SelectBehavior.Select }
+      else
+        fallback()
+      end
+    end,
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" },
+    { name = "latex_symbols" },
+  },
+  formatting = {
+    format = function(entry, vim_item)
+      -- fancy icons and a name of kind
+      vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
 
-            vim_item.menu = ({
-                buffer = "[Buffer]",
-                nvim_lsp = "[LSP]",
-                luasnip = "[Snippet]",
-                nvim_lua = "[Lua]",
-                latex_symbols = "[Latex]",
-            })[entry.source.name]
-            return vim_item
-        end,
-    },
-    experimental = {
-        ghost_text = false, -- this feature conflict with copilot.vim's preview.
-    },
+      vim_item.menu = ({
+        buffer = "[Buffer]",
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
+        nvim_lua = "[Lua]",
+        latex_symbols = "[Latex]",
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
+  experimental = {
+    ghost_text = false, -- this feature conflict with copilot.vim's preview.
+  },
 }
 -- If you want insert `(` after select function or method item
 -- local cmp_autopairs = require "nvim-autopairs.completion.cmp"
