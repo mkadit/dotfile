269b2d031d53bebda3d6b0b31f36c3522e0fbcd1
mkadit <mkaditya00@gmail.com>
2022-07-03 10:36:43 +0700

    chore: Add deps for testing

--- .config/nvim/lua/editor/neotest.lua
+++ .config/nvim/lua/editor/neotest.lua
@@ -1,13 +1,20 @@
-require("neotest").setup({
+require("neotest").setup {
   adapters = {
-    require("neotest-python")({
+    require "neotest-python" {
       dap = { justMyCode = false },
-      runner = "pytest"
-    }),
+      runner = "pytest",
+    },
     -- require("neotest-plenary"),
-    require("neotest-go"),
-    -- require("neotest-vim-test")({
+    require "neotest-go",
+    -- require("neotest-jest")({
+    --   jestCommand = "npm test --"
+    -- })
+    -- require "neotest-vim-test" {
     --   ignore_file_types = { "python", "vim", "lua" },
-    -- }),
+    -- },
   },
-})
+}
+
+-- vim.cmd [[
+-- let g:test#javascript#runner = 'jest'
+-- ]]
